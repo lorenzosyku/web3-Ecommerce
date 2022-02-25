84e0c6978d147b47a9cdcece01abcac53ca7144e
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-24 22:07:38 -0500

    transfer functionality implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,30 @@ import { useMoralis } from "react-moralis";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { getFirestore, collection } from "@firebase/firestore";
 import { db } from "../firebase";
+import { useState } from "react";
 
 export default function Home() {
+  const { Moralis, authenticate, isAuthenticated, logout } = useMoralis();
   const [snapshot, loading, error] = useCollection(collection(db, "products"));
+  const [errmsg, setErrmsg] = useState();
 
-  console.log(snapshot);
+  type TransferType = Parameters<typeof Moralis.transfer>[0];
+  const purchase = async (price: number) => {
+    const options: TransferType = {
+      type: "native",
+      amount: Moralis.Units.ETH(price),
+      receiver: "0xac712CA6b82e2eE44df82e4730D4f53Aeff874f1",
+    };
+
+    Moralis.transfer(options)
+      .then((transaction) => {
+        console.log(transaction);
+      })
+      .catch((error) => {
+        console.log(error.data.message);
+      });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 space-y-10">
       <Head>
@@ -15,17 +34,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="text-3xl">Welcome to the Web3 Store</h1>
-      <div className="">
-        {snapshot?.docs.map((doc) => (
-          <div
-            key={doc.id}
-            className="flex transform cursor-pointer justify-between space-x-8 p-5 shadow-md transition duration-200 hover:scale-105"
-          >
-            <p>{doc.data().name}</p>
-            <p>{doc.data().price}</p>
+      {!isAuthenticated ? (
+        <button
+          onClick={() => {
+            authenticate();
+          }}
+        >
+          Login
+        </button>
+      ) : (
+        <div className="">
+          {snapshot?.docs.map((doc) => (
+            <div
+              key={doc.id}
+              className="flex transform cursor-pointer justify-between space-x-8 p-5 shadow-md transition duration-200 hover:scale-105"
+              onClick={()=>purchase(doc.data().price)}
+            >
+              <p>{doc.data().name}</p>
+              <p>{doc.data().price} ETH</p>
+            </div>
+          ))}
+          <div className="p-5">
+            <h3 className="text-red-400">{errmsg}</h3>
           </div>
-        ))}
-      </div>
+          <button
+            onClick={() => {
+              logout();
+            }}
+          >
+            logout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
