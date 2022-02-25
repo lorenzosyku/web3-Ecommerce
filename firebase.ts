ad9ce677484918db12d5653469dab9f6179a59c6
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-24 20:59:49 -0500

    added function that get the products from firebase

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApps, getApp, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,4 +19,4 @@ const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
-export { db }
+export { db, app }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useMoralis } from "react-moralis";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { getFirestore, collection } from "@firebase/firestore";
+import { db } from "../firebase";
 
 export default function Home() {
+  const [snapshot, loading, error] = useCollection(collection(db, "products"));
+
+  console.log(snapshot);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 space-y-10">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h2>hello</h2>
+      <h1 className="text-3xl">Welcome to the Web3 Store</h1>
+      <div className="">
+        {snapshot?.docs.map((doc) => (
+          <div
+            key={doc.id}
+            className="flex transform cursor-pointer justify-between space-x-8 p-5 shadow-md transition duration-200 hover:scale-105"
+          >
+            <p>{doc.data().name}</p>
+            <p>{doc.data().price}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
